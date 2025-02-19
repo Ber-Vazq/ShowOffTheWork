2919ec542ce4258682fc345febc2072eabb739e4
Ber-Vazq <bvaq20@hotmail.com>
2025-02-19 14:24:50 -0600

    smooth scroll

--- portfolio.js
+++ portfolio.js
@@ -12,3 +12,18 @@ const observer = new IntersectionObserver((entries) => {
 document.querySelectorAll('[data-effect]').forEach(el => {
     observer.observe(el);
 });
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+      e.preventDefault();
+      const target = document.querySelector(this.getAttribute('href'));
+      const headerOffset = 80; // Your navbar height
+      const elementPosition = target.offsetTop;
+      const offsetPosition = elementPosition - headerOffset;
+  
+      window.scrollTo({
+        top: offsetPosition,
+        behavior: 'smooth'
+      });
+    });
+  });
